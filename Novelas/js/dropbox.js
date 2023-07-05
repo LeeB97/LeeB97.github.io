f53721889f81128dc9fad6f45cff615230c67984
Nayem RS <50403736+LeeB97@users.noreply.github.com>
2023-07-05 12:57:14 +0100

    Update dropbox.js

--- Novelas/js/dropbox.js
+++ Novelas/js/dropbox.js
@@ -1,6 +1,6 @@
 'use strict';
-var dbx = new Dropbox.Dropbox({ accessToken: 'sl.BhmDlXlf4LzztU_IPTaarxew_dWGprST_KkO01BhdmQl-m3P5K_KIAVD5pA6CV7JEet7pwdv-iS23nC8rDFifYyiFLvKlygeVKoofa5-ToF85P-PQXiwrfW9R3I4mBluIEaI_MP-' });
-
+const token = 'sl.BhmCGwxs1qf0jSYacEjZ7t3cczlLMxHc7wPR_EYseXxJBZdljXSwX3gENTVBS5KzBRzgO60H177wahCCg59dX4YmJbkhY6zsYLHsBxjUMpkY-ol-ILifBYWppoYqJCKGxLJTEIA6qdVM';
+var dbx = new Dropbox.Dropbox({ accessToken: token });
 dbx.sharingGetSharedLinkFile({url: 'https://www.dropbox.com/s/8ndtu5xb7gr6j2p/index.html?dl=0'})
   .then(function(data){
     const reader = new FileReader();
