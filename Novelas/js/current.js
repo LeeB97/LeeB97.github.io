2262dee4cf4b44b00fb326e30666f7b4ca06356d
Nayem RS <50403736+LeeB97@users.noreply.github.com>
2023-07-25 17:36:19 +0100

    Update current.js

--- Novelas/js/current.js
+++ Novelas/js/current.js
@@ -1,6 +1,6 @@
 'use strict';
 const synth = window.speechSynthesis,
-volume = sessionStorage.getItem("voice-volume") ?? 1,
+volume = sessionStorage.getItem("voice-volume") ?? 0.95,
 rate = sessionStorage.getItem("voice-rate") ?? 1, //1.1161,
 pitch = sessionStorage.getItem("voice-pitch") ?? 1,
 share_link = "https://dl.dropboxusercontent.com/s/8ndtu5xb7gr6j2p/index.html?dl=0",
